88440b4e5bc9f4f88dc1d3c8c337ecca50cc042d
Gustavo Rodrigues <rmmgustavo@gmail.com>
2023-08-08 10:51:11 -0300

    index e outros

---
+++ script.js
@@ -0,0 +1,20 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  const img = document.querySelector("#profile img")
+  
+  if(html.classList.contains('light')) {
+    img.setAttribute("src", "./assets/assets/original-light.jpeg")
+    img.setAttribute(
+      "alt",
+      "foto "
+    )
+  } else {
+    img.setAttribute("src", "./assets/assets/original.jpeg")
+    img.setAttribute(
+      "alt",
+      "foto da grandioza"
+    )
+  }
+}
